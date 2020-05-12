229dc8310ca3f1994fdfa9a549b0908c6b41187d
Arghyadeep <arghyadeep.k@gmail.com>
2020-05-13 00:20:41 +0530

    Updated function name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function vance(str, varList) {
+module.exports = function vanes(str, varList) {
     let data = null;
     for(var x in varList) {    
         data = str.replace('{' + x + '}', varList[x]);
